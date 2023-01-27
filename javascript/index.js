26f4669a690a7bba3c159ac3f6d404e8be058bff
kevin rey <kvinjr9@gmail.com>
2023-01-27 15:49:31 -0500

    COMMIT INICIAL

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+//variables var, const y let son variables y la variable const no combia si numeracion
+
+
+//console.log("hola locos");
+
+//var numero=5;
+
+//console.log(numero);
+//const Minumero =14
+
+//var opt1= 50
+//var opt2= 90
+//var suma = opt1+opt2
+
+//console.log(suma)
+
+//let Mimane = 3+3
+//console.log("el nuemro es " + Mimane)
+//Condicionales if y else
+
+
+//bucles while and for
+
+//let contador = -5
+//while (contador <100){
+//  console.log(contador)
+// contador=contador + 1
+//}
+
+// for (let i =0 ;i<=40; i++){
+//console.log(i)
+//}
+
